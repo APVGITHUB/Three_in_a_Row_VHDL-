67fac32d6e862daf5e7318401f37a3eb7b334693
guillee35 <49530604+guillee35@users.noreply.github.com>
2019-12-12 12:12:17 +0100

    Update turno.vhd

--- turno.vhd
+++ turno.vhd
@@ -2,66 +2,111 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+entity tb_turno is
+end tb_turno;
 
-entity turno is
-    port (
-        clk: in std_logic;
-        reset: in std_logic;
-        B1: in std_logic;
-        B1_prev: in std_logic;
-        V1: in std_logic;
-        V2: in std_logic;
-        E: in std_logic;
-        comparar: in std_logic_vector (8 downto 0);
- --       election: in std_logic_vector (8 downto 0);           -- eleccion => mantener parpadeo simepre (se pone a cero al comienzo de cada turno)
-        turn: out std_logic                                   -- 0 => turno jugador 1        -- 1 => turno jugador 2
-   
-           
-);
-end turno;
-
-architecture Behavioral of turno is
-    signal turno: std_logic;
-    signal lfsr: std_logic_vector (3 downto 0) := "0000";
-    signal feedback: std_logic;
-    signal fin: std_logic;
+architecture Behavioral of tb_turno is
     
+    component turno is
+        port(
+            clk: in std_logic;
+            reset: in std_logic;
+            B1: in std_logic;
+            B1_prev: in std_logic;
+            comparar: in std_logic_vector (8 downto 0);
+            V1: in std_logic;
+            V2: in std_logic;
+            E: in std_logic;
+        --    election: in std_logic_vector (8 downto 0);  
+            turn: out std_logic       
+        );
+    end component;
+    
+     signal clk: std_logic;
+     signal reset: std_logic;
+     signal B1: std_logic;
+     signal B1_prev: std_logic;
+     signal comparar: std_logic_vector (8 downto 0);
+     signal V1: std_logic;
+     signal V2: std_logic;
+     signal E: std_logic;
+   --  signal election: std_logic_vector (8 downto 0);  
+     signal turn: std_logic;
+     
+     --declaracion tiempo
+     constant clk_period : time := 8 ns;
+            
 begin
 
-feedback <= not(lfsr(3) xor lfsr(2)); 
---process(clk,reset)
---begin
---    if reset = '1' then
---        lfsr <= (others => '0');
---    elsif clk'event and clk = '1' then
---        lfsr <= lfsr(2 downto 0) & feedback;
---    end if;
---end process;
+    --instanciacion componente
+    uut: turno
+    port map (
+        clk => clk,
+        reset => reset,
+        B1 => B1,
+        B1_prev => B1_prev,
+        comparar => comparar,
+        V1 => V1,
+        V2 => V2,
+        E => E,
+  --      election => election,
+        turn => turn
+        );
+        
+     --generacion de reloj
+      process
+      begin
+          clk <= '1';
+          wait for clk_period/2;
+          clk <= '0';
+          wait for clk_period/2;        
+      end process;
 
-lfsr <= lfsr(2 downto 0) & feedback;
-
---turno inicial
-fin <= V1 or V2 or E;
-process(clk,reset)
-begin
-    if reset = '1' then
-        turno <= '0';
-        --inicio <= '0';
-    elsif clk'event and clk = '1' then 
-        if fin = '1' then         
-            if V1 = '1' then
-                turno <= '1';
-            elsif V2 = '1' then
-                turno <= '0';
-            elsif E <= '1' then
-                turno <= lfsr(2);
-            end if;
-        elsif B1 = '0' and B1_prev = '1' and comparar = "000000000" then
-            turno <= not(turno);
-        end if;
-    end if;
-end process;
-
-turn <= std_logic(turno);
+         --generacion resto de entradas
+       process
+       begin
+           reset <= '1';
+           B1 <= '0';
+           B1_prev <= '0';
+           comparar <= (others => '0');
+           V1 <= '0';
+           V2 <= '0';
+           E <= '0';
+        --   election <= (others => '0');
+           wait for clk_period*200;
+           reset <= '0';
+           wait for clk_period*10;
+      --     election <= "000000001";
+           wait for clk_period*10;
+           B1 <= '1';
+           wait for clk_period;
+           B1_prev <= '1';
+           wait for clk_period*10;
+           B1 <= '0';
+           wait for clk_period;
+           B1_prev <= '0';
+           wait for clk_period*10;
+    --       election <= "000000010";
+           wait for clk_period*10;
+           B1 <= '1';
+           wait for clk_period;
+           B1_prev <= '1';
+           wait for clk_period*10;
+           B1 <= '0';
+           wait for clk_period;
+           B1_prev <= '0';
+           wait for clk_period*10;
+     --      election <= "000000100";
+           wait for clk_period*10;
+           B1 <= '1';
+           wait for clk_period;
+           B1_prev <= '1';
+           wait for clk_period*10;
+           B1 <= '0';
+           V1 <= '1';
+           wait for clk_period;
+           B1_prev <= '0';
+           wait;
+        end process;           
 
 end Behavioral;
