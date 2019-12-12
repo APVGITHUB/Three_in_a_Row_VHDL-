c865d1603cdfb964b5127f27016421da51b69108
guillee35 <49530604+guillee35@users.noreply.github.com>
2019-12-12 12:16:04 +0100

    Update validar.vhd

--- validar.vhd
+++ validar.vhd
@@ -13,160 +13,119 @@ entity validar is
         clk: in std_logic;
         reset: in std_logic;
         B1: in std_logic;
-        B1_prev: in std_logic;
-        V1: in std_logic;
-        V2: in std_logic;
-        E: in std_logic;
-        election: in std_logic_vector (8 downto 0);         -- eleccion => mantener parpadeo simepre (se pone a cero al comienzo de cada turno)
---        Tablero1: out std_logic_vector (8 downto 0);        -- Tablero jugador 1 => mantenerlo encendido siempre (se va actualizando solo)
---        Tablero2: out std_logic_vector (8 downto 0);        -- Tablero jugador 2 => mantenerlo encendido siempre (se va actualizando solo)
---         fail: out std_logic;                               -- si la posicion elegida no es valida => fail=1 durante 1 segundo
-        turn: out std_logic_vector( 1 downto 0)              -- 01 => turno jugador 1        -- 10 => turno jugador 2
-       
+        B1_prev1: in std_logic;
+        B1_prev2: in std_logic;
+        turno: in std_logic;
+        election: in std_logic_vector (8 downto 0);           -- eleccion => mantener parpadeo simepre (se pone a cero al comienzo de cada turno)
+        Tablero1: out std_logic_vector (8 downto 0);        -- Tablero jugador 1 => mantenerlo encendido siempre (se va actualizando solo)
+        Tablero2: out std_logic_vector (8 downto 0);        -- Tablero jugador 2 => mantenerlo encendido siempre (se va actualizando solo)
+--        fail: out std_logic;                               -- si la posicion elegida no es valida => fail=1 durante 1 segundo
+        V1: out std_logic;      --victoria jugador 1
+        V2: out std_logic;      --victoria jugador 2
+        E: out std_logic        --empate
                
     );
 end validar;
 
 architecture Behavioral of validar is
     
---    --determinar eleccion 
---    signal comparar: std_logic_vector(8 downto 0);
---    constant MAX: integer := 125*10**6;
---    signal seg: integer range 0 to MAX-1;
---    signal fijar: std_logic;
---    signal error: std_logic;
---    signal Tab1: std_logic_vector (8 downto 0) := "000000000";
---    signal Tab2: std_logic_vector (8 downto 0) := "000000000";
---    signal ocho: std_logic;
+    --determinar eleccion 
+    signal comparar: std_logic_vector(8 downto 0) := "000000000";
+    constant MAX: integer := 125*10**6;
+    signal seg: integer range 0 to MAX-1;
+
+    signal Tab1: std_logic_vector (8 downto 0) := "000000000";
+    signal Tab2: std_logic_vector (8 downto 0) := "000000000";
+
+    signal S: std_logic_vector (8 downto 0);
+    signal V: std_logic;    --victoria
+    signal Tab: std_logic_vector (8 downto 0);      --fichas de 1 + fichas de 2
+    signal empate: std_logic;
+    signal error: std_logic;
+    signal ocho: std_logic;
     
-    signal turno: unsigned (1 downto 0) := "00";
-    --signal inicio: std_logic;
-    
-    signal lfsr: std_logic_vector (3 downto 0);
-    signal feedback: std_logic;
-    signal r: unsigned (1 downto 0);
 
 begin
 
-----comparacion del tablero actual con la posicion nueva elegida
+Tab <= Tab1 xor Tab2;
+--comparacion del tablero actual con la posicion nueva elegida
 --process(clk,reset)
 --begin
 --    if reset = '1' then
 --        comparar <= "000000000";
 --    elsif clk'event and clk = '1' then
---        comparar <= eleccion and(Tab1 xor Tab2);
+--        comparar <= election and Tab;
 --    end if;
 --end process;
+comparar <= election and Tab;
 
---fijar <= '1' when comparar="000000000" and B1='1' and B1prev='0' else '0';      --dura un pulso de reloj
---error <= '1' when comparar/="000000000" and B1='1' and B1prev='0' else '0';     --dura un pulso de reloj
-
-----activar error(ocho) 1 segundo 
---process(clk,reset)
---begin
---    if reset = '1' then
---        ocho <= '0';
---    elsif clk'event and clk = '1' then
---        if error = '1' then
---            ocho <= '1';
---        end if;
---    end if;
---end process;
-
-----contador de un segundo        
---process(clk,reset)
---begin
---    if reset = '0' then
---        seg <= 0;
---    elsif clk'event and clk = '1' then
---        if ocho = '1' then
---            if seg = MAX-1 then
---                seg <= 0;
---                ocho <= '0';
---            else 
---                seg <= seg + 1;
---            end if;
---        end if;
---    end if;
---end process;
-
-----actualizar tablero
---Tab1 <= (Tab1 xor eleccion) when (turno = '0' and fijar = '1');
---Tab2 <= (Tab2 xor eleccion) when (turno = '1' and fijar = '1');
-
-----pulsar boton B1 y eleccion es válida => fijar = '1'
---process(clk,reset)
---begin
---    if reset = '1' then
---        B1prev <= '0';
---    elsif clk'event and clk = '1' then
---        if B1 = '1' then
---            B1prev <= '1';
---        end if;
---    end if;
---end process;
-
-----soltar boton B1 y cambia turno
---process(clk,reset)
---begin
---    if reset = '1' then
---        --turno <= alterna;       --primer turno de la partida
---    elsif clk'event and clk = '1' then
---        if B1='0' and B1prev='1' then
---            if turno = '0' then turno <= '1';     -- turno 1 --> turno 2
---            elsif turno = '1' then turno <= '0';  -- turno 2 --> turno 1
---            end if;
---            B1prev <= '0';
---        end if;
---    end if;
---end process;
+--actualizar tablero
+Tab1 <= (Tab1 xor election) when (turno = '0' and B1 = '1' and comparar = "000000000");
+Tab2 <= (Tab2 xor election) when (turno = '1' and B1 = '1' and comparar = "000000000");
+--activar error 1 ciclo de reloj
+error <= '1' when (B1 = '1' and B1_prev1 = '0' and comparar /= "000000000") else '0';
 
 
---bit aleatorio 
-feedback <= not(lfsr(3) xor lfsr(2)); 
+--MUX
+with turno select
+    S <= Tab1 when '0',
+         Tab2 when '1',
+         "---------" when others;
+         
+--empate
+empate <= '1' when (Tab = "111111111") and V/= '0' else '0';     --debe durar un ciclo de reloj
+
+
+--3 en raya
+V <= '1' when (S(0)='1' and S(1)='1' and S(2)='1') or 
+              (S(3)='1' and S(4)='1' and S(5)='1') or 
+              (S(6)='1' and S(7)='1' and S(8)='1') or 
+              (S(0)='1' and S(3)='1' and S(6)='1') or 
+              (S(1)='1' and S(4)='1' and S(7)='1') or 
+              (S(2)='1' and S(5)='1' and S(8)='1') or 
+              (S(2)='1' and S(4)='1' and S(6)='1') or 
+              (S(0)='1' and S(4)='1' and S(8)='1')
+              else '0'; 
+
+
+--enable contador de un segundo
 process(clk,reset)
 begin
     if reset = '1' then
-        lfsr <= (others => '0');
+        ocho <= '0';
     elsif clk'event and clk = '1' then
-        lfsr <= lfsr(2 downto 0) & feedback;
+        if error = '1' then
+            ocho <= '1';
+        end if;
     end if;
 end process;
 
-with lfsr(3) select
-r <= "01" when '0',
-     "10" when '1',
-     "--" when others;
-
-----turno inicial
---process(clk,reset)
---begin
---    if reset = '1' then
---        turno <= (others => '0');
---        --inicio <= '0';
---    elsif clk'event and clk = '1' then          
---        if V1 = '1' then
---            turno <= "10";
---        elsif V2 = '1' then
---            turno <= "01";
---        elsif E <= '1' then
---            turno <= r;
---        else
---            if B1 = '0' and B1_prev = '1' then
---                turno <= not(turno);
---            end if;
---        end if;
---    end if;
---end process;
-turno <= "01" when V1 = '1' else
-         "10" when V2 = '1' else
-         r when E = '1' else
-         not(turno) when B1 = '0' and B1_prev = '1' else
-         "--";
+--contador de un segundo        
+process(clk,reset)
+begin
+    if reset = '1' then
+        seg <= 0;
+        --fail <= '0';
+    elsif clk'event and clk = '1' then
+        if ocho = '1' then
+            if seg = MAX-1 then
+                seg <= 0;
+                ocho <= '0';
+            else 
+                seg <= seg + 1;
+            end if;
+        end if;
+        --fail <= ocho;
+    end if;
+end process;
 
 --salidas
---Tablero1 <= std_logic_vector(Tab1);
---Tablero2 <= std_logic_vector(Tab2);
-turn <= std_logic_vector(turno);
---fail <= ocho;
+Tablero1 <= std_logic_vector(Tab1);
+Tablero2 <= std_logic_vector(Tab2);
+
+--V1 y V2 tienen que ser un pulso de reloj
+V1 <= '1' when (V = '1' and turno = '0' and B1='1' and B1_prev1 ='0') else '0';
+V2 <= '1' when (V = '1' and turno = '1' and B1='1' and B1_prev1 ='0') else '0';
+E <= '1' when (empate = '1' and B1='1' and B1_prev1='0') else '0';
+
 end Behavioral;
