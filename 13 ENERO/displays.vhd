d5eca643e2fdaa073f53aef99777c5110104bccd
guillee35 <49530604+guillee35@users.noreply.github.com>
2020-01-15 10:56:25 +0100

    Update displays.vhd

--- 13 ENERO/displays.vhd
+++ 13 ENERO/displays.vhd
@@ -5,24 +5,21 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Displays is
   Port (
-        clk: in std_logic;
         reset: in std_logic;
-        speed: in std_logic;
+        clk: in std_logic;
         selector: out std_logic_vector (3 downto 0);
         segmentos: out std_logic_vector (7 downto 0);
         election: in std_logic_vector (8 downto 0);        -- eleccion => mantener parpadeo simepre (se pone a cero al comienzo de cada turno)
         Tablero1: in std_logic_vector (8 downto 0);            -- Tablero jugador 1 => mantenerlo encendido siempre (se va actualizando solo)
         Tablero2: in std_logic_vector (8 downto 0);            -- Tablero jugador 2 => mantenerlo encendido siempre (se va actualizando solo)
-        turn: in std_logic;           -- 01 => turno jugador 1        -- 10 => turno jugador 2
+        turn: in std_logic_vector (1 downto 0);           -- 01 => turno jugador 1        -- 10 => turno jugador 2
         fail: in std_logic;
         
         V1: in std_logic;      --victoria jugador 1
         V2: in std_logic;      --victoria jugador 2
         E: in std_logic;
         gana1: in std_logic;
-        gana2: in std_logic;
-        parar_temporizador: out std_logic
-        );
+        gana2: in std_logic);
            
 end Displays;
 
@@ -34,26 +31,24 @@ architecture Behavioral of Displays is
     signal V25s: std_logic;
     signal E5s: std_logic;
     
-    signal divider: integer range 1 to 1000;
-    constant MaxDFSel: integer := 125000/5; -- Div.Freq 5kHz  
-    signal count_DFSel: integer range 0 to MaxDFSel-1;
+    constant MaxDFSel: integer := 125000/5-1; -- Div.Freq 5kHz
+    signal count_DFSel: integer range 0 to MaxDFSel;
     signal DFSel: std_logic;
     signal sel: integer range 0 to 3;
     ---------------------------------------------------------------------
-    constant MaxDFTab: integer := 125000/500; --Div.Freq 500 kHz
-    constant MaxTab: integer := 63;
-    signal count_DFTab: integer range 0 to MaxDFTab-1;
+    constant MaxDFTab: integer := 125000/500-1; --Div.Freq 500 kHz
+    signal count_DFTab: integer range 0 to MaxDFTab;
     signal DFTab: std_logic;
-    signal tab: integer range 0 to MaxTab;
+    signal tab: integer range 0 to 1;
     -------------------------------------------------------------------
     -- Divisor de Frecuencia que se usa para el selector de displays cuando hay que mostrar 1,2 o  = durante 5 segs
-    constant MaxDF5: integer := 125000000/5; --Div.Freq 5 Hz
-    signal count_DF5: integer range 0 to MaxDF5-1;
+    constant MaxDF5: integer := 125000000/5-1; --Div.Freq 5 Hz
+    signal count_DF5: integer range 0 to MaxDF5;
     signal DF5: std_logic;
     signal countsel5: integer range 0 to 3;
     --------------------------------------------------------
     signal tablero: std_logic_vector (8 downto 0);
-    signal col_tablero: std_logic_vector (2 downto 0); --División de tablero en la parte correspondiente a cada display
+    signal col_tablero: std_logic_vector (2 downto 0); --DivisiÃ³n de tablero en la parte correspondiente a cada display
    
     signal sel_election: integer range 0 to 3; -- Registra en que display se tiene que controlar el parpadeo/fail del segmento elegido para colocar la ficha
     
@@ -68,15 +63,13 @@ architecture Behavioral of Displays is
 
         
 begin
---divisor de frecuencia para la simulacion
-divider <= 1000 when speed = '1' else 1;
 -------------------------------------------------------------------------------
 DivFreqSel: process(clk, reset) -- 5 kHz
     begin
         if reset = '1' then
             count_DFSel <= 0;
         elsif (clk' event and clk = '1') then
-            if count_DFSel = MaxDFSel/divider-1 then
+            if count_DFSel = MaxDFSel then
                 count_DFSel <= 0;
             else
                 count_DFSel <= count_DFSel + 1;
@@ -84,7 +77,7 @@ DivFreqSel: process(clk, reset) -- 5 kHz
         end if;
     end process;
     
- DFSel <= '1' when count_DFSel = MaxDFSel/divider-1 else '0';
+ DFSel <= '1' when count_DFSel = MaxDFSel else '0';
  
 PermSelector:process(clk, reset) -- Permuta a 0,1,2,3 el contador sel que controla (multiplexa) el selector de displays
      begin
@@ -104,12 +97,12 @@ PermSelector:process(clk, reset) -- Permuta a 0,1,2,3 el contador sel que contro
 DivFreqTab: process(clk, reset)
     begin
       if reset = '1' then
-        count_DFTab <= 1;
+        count_DFTab <= 0;
       elsif (clk' event and clk = '1') then
         if DFSel = '1' then
            count_DFTab <= 0;
         else
-         if count_DFTab = MaxDFTab/divider-1 then
+         if count_DFTab = MaxDFTab then
            count_DFTab <= 0;
          else
            count_DFTab <= count_DFTab + 1;
@@ -118,15 +111,13 @@ DivFreqTab: process(clk, reset)
       end if;
      end process;
      
-DFTab <= '1' when count_DFTab = 0  else '0';     
+DFTab <= '1' when count_DFTab = MaxDFTab else '0';     
     
 PermTab:process(clk, reset) -- permuta el tablero que se debe mostrar entre el tablero1 y el tablero2
    begin
-     if reset = '1' then
-        tab <= 0;
-     elsif (clk' event and clk = '1') then
+     if (clk' event and clk = '1') then
         if DFTab = '1' then
-            if tab = MaxTab then
+            if tab = 1 then
                tab <= 0;
             else
                tab <= tab + 1;
@@ -142,7 +133,7 @@ begin
     if reset='1' then
         count_DF5 <= 0;
     elsif (clk' event and clk='1') then
-        if count_DF5 = MaxDF5/divider-1 then
+        if count_DF5 = MaxDF5 then
             count_DF5 <= 0;
         else
             count_DF5 <= count_DF5 +1;
@@ -150,13 +141,13 @@ begin
     end if;
  end process;
  
- DF5 <= '1' when count_DF5 = MaxDF5/divider-1 else '0';
+ DF5 <= '1' when count_DF5 = MaxDF5 else '0';
  
  PermSelector5Hz: process(reset, clk)
  begin
     if reset='1' then 
         countsel5 <= 0;
-        segmentos_electioncmp <= "00000000";--ORIGINALMENTE TODO UNOS, PERO CREO QUE DEBE SER CON CEROS
+        segmentos_electioncmp <= "11111111";
     elsif (clk' event and clk='1') then
         if DF5='1' then
             if countsel5 = 3 then -- Permuta el valor del selector de displays para el modo resultado (1,2,= durante 5s)
@@ -165,8 +156,8 @@ begin
                 countsel5 <= countsel5 + 1;
             end if;
             
-            if segmentos_electioncmp = "11111111" then -- Permuta el valor de una señal de comparacion para hacer parpadear solo el segmento de election en el modo tablero
-                segmentos_electioncmp <= (segmentos_election);
+            if segmentos_electioncmp = "11111111" then -- Permuta el valor de una seÃ±al de comparacion para hacer parpadear solo el segmento de election en el modo tablero
+                segmentos_electioncmp <= not(segmentos_election);
             else
                 segmentos_electioncmp <= "11111111";
             end if;
@@ -230,11 +221,8 @@ selector_5s <= "0001" when 0,
                "1000" when 3;
                
 with tab select
-tablero <=  (Tablero2 or tablero1) when 0,
-            (Tablero2 or tablero1) when 1,
-            (Tablero2 or tablero1) when 2,
-            (Tablero2 or tablero1) when 3,
-            Tablero1 when others;
+tablero <= Tablero1 when 0,
+           Tablero2 when 1;
 
 with sel select         
 col_tablero <=  tablero(8 downto 6) when 1, 
@@ -243,20 +231,20 @@ col_tablero <=  tablero(8 downto 6) when 1,
                 "---" when others;
                 
 with col_tablero select 
-    segmentos_tablero <= "01101101" when "111",
-                         "01111111" when "100",
-                         "01111101" when "110",
-                         "11111101" when "010",
-                         "11101101" when "011",
-                         "11101111" when "001",
-                         "01101111" when "101",
-                         "11111111" when "000",
-                         "11111111" when others;
+    segmentos_tablero <= "10010010" when "111",
+                         "10000000" when "100",
+                         "10010000" when "110",
+                         "00010000" when "010",
+                         "00010010" when "011",
+                         "00000010" when "001",
+                         "10000010" when "101",
+                         "00000000" when "000",
+                         "00000000" when others;
                          
 with turn select
-    segmentos_turn <= "10011111" when '0',
-                      "00100101" when '1',
-                      "11111111" when others;
+    segmentos_turn <= "01100000" when "01",
+                      "11011010" when "10",
+                      "00000000" when others;
                       
 with sel select
     segmentos_partida <= segmentos_tablero when 1,
@@ -278,29 +266,27 @@ with election select
                     0 when others;
 
 with election select
-    segmentos_election <= "01111111" when "000000100",
-                          "01111111" when "000100000",
-                          "01111111" when "100000000",
-                          "11111101" when "000000010",
-                          "11111101" when "000010000",
-                          "11111101" when "010000000",
-                          "11101111" when "000000001",
-                          "11101111" when "000001000",
-                          "11101111" when "001000000",
-                          "11111111" when others;
+    segmentos_election <= "10000000" when "000000100",
+                          "10000000" when "000100000",
+                          "10000000" when "100000000",
+                          "00010000" when "000000010",
+                          "00010000" when "000010000",
+                          "00010000" when "010000000",
+                          "00000010" when "000000001",
+                          "00000010" when "000001000",
+                          "00000010" when "001000000",
+                          "--------" when others;
                     
-segmentos <= "10011111" when (finjuego='1' and gana1='1')else
-             "00100101" when (finjuego='1' and gana2='1')else
-             "10011111" when (temp5s='1' and V15s='1')else
-             "00100101" when (temp5s='1' and V25s='1')else
-             "01101111" when (temp5s='1' and E5s='1')else
-             "00000001" when (fail='1' and sel_election=sel) else
-             --ABAJO EL OR ERA UN AND, PERO NO FUNCIONABA
-             (segmentos_partida and segmentos_electioncmp) when (election/="000000000" and sel_election=sel) else
+segmentos <= "01100000" when (finjuego='1' and gana1='1')else
+             "11011010" when (finjuego='1' and gana2='1')else
+             "01100000" when (temp5s='1' and V15s='1')else
+             "11011010" when (temp5s='1' and V25s='1')else
+             "10000010" when (temp5s='1' and E5s='1')else
+             "11111110" when (fail='1' and sel_election=sel) else
+             segmentos_partida and segmentos_electioncmp when (election/="000000000" and sel_election=sel) else
              segmentos_partida;
              
 selector <= selector_5s when temp5s='1' else
             selector_partida;
 
-parar_temporizador <= temp5s;
 end Behavioral;
